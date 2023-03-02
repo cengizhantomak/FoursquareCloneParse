575205d4e8fe7186415e0afc9d54616ba1ce7043
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-02 17:27:03 +0300

    Parse Veri Kaydetmek

--- FoursquareCloneParse/ViewController.swift
+++ FoursquareCloneParse/ViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 import ParseSwift
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Apple"
+        parseObject["calories"] = 100
+        parseObject.saveInBackground { success, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print("uploaded")
+            }
+        }
+        
     }
 
 
