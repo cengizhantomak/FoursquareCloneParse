438663aecb43760aac5db06ae31fd71ef2bc828f
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-02 14:20:20 +0300

    Initial Commit

---
+++ FoursquareCloneParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareCloneParse
+//
+//  Created by Kerem Tuna Tomak on 2.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
