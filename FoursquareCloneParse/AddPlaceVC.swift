a3f2cd8f79e423a21bf1e802f979dabe03ae52a3
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-02 20:20:06 +0300

    Mekan Ekleme VC

---
+++ FoursquareCloneParse/AddPlaceVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddPlaceVC.swift
+//  FoursquareCloneParse
+//
+//  Created by Kerem Tuna Tomak on 2.03.2023.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+
+    @IBOutlet weak var placeNameText: UITextField!
+    @IBOutlet weak var placeTypeText: UITextField!
+    @IBOutlet weak var placeAtmosphereText: UITextField!
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func nextButtonClicked(_ sender: Any) {
+    }
+    
+}
