ebaeb56d3ea330a465609d58e5188f285b0e5d44
Cengizhan Tomak <cengizhantomak@gmail.com>
2023-03-02 14:59:05 +0300

    Sunucu Baglantisi

--- FoursquareCloneParse/AppDelegate.swift
+++ FoursquareCloneParse/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { ParseMutableClientConfiguration in
+            ParseMutableClientConfiguration.applicationId = "pQdP6Rh3dTYYmzy3tvPiRvZfq6wwjwYbMoY2SKkW"
+            ParseMutableClientConfiguration.clientKey = "W0MR54scwQ5ThIoi503YTuaYfKOTgbUKYaHhqw9L"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+        
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
